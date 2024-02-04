fc163bb0687beb199bc1a2747b6bce43f0b373b2
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2024-02-04 00:32:18 -0700

    placeholder routing

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.css";
+import Budget from "./pages/Budget";
+import Education from "./pages/Education";
+import Expenses from "./pages/Expenses";
+import Loading from "./pages/Loading";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import Settings from "./pages/Settings";
 
-function app() {
+function App() {
   return (
-    <div>app</div>
-  )
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/budget" element={<Budget />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/expenses" element={<Expenses />} />
+          <Route path="/loading" element={<Loading />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default app
+export default App;
