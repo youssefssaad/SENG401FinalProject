a67ce1b1b83f97b89590916f1dc68799588fc52a
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2024-02-10 16:56:40 -0700

    styling for the whole program

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React , {useState} from 'react';
 
-function Login() {
+const Login = () => {
+  const [input, setInput] = useState({
+    username: "",
+    password: ""
+  })
+
+  const handleSubmit = () => {
+    if (input.username !== "" && input.password !== "") {         // placeholder. databse authentication would be here
+    alert(`What's good ${input.username}?!`);
+    } else {
+      alert("Lock in bro");
+  };
+  }
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
-    <div>login</div>
+    <form onSubmit = {handleSubmit}>
+      <input type="text" name="username" placeholder="Username" onChange={handleInput}/>
+      <input type="password" name="password" placeholder="Password" onChange={handleInput}/>
+      <button type="submit">Login</button>
+      <p className='registeration'> Don't got an account?</p> 
+      <a href="/register" type='registration_link'>Sign up here!</a>
+    </form>
   );
 };
 
