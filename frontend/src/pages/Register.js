0f3892dcdff53fb239dc93ffc27a09043e383c5c
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2024-02-10 17:21:47 -0700

    console logging

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -18,40 +18,43 @@ const Register = () => {
 
   const passwordMatch = () => input.passwordCheck === input.password;
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (input.username !== "" && input.password !== "" && input.email !== "" && input.passwordCheck !== "" && passwordMatch()) {  
-      const formData = {
-        email: input.email,
-        username: input.username,
-        password: input.password,
-        passwordCheck: input.passwordCheck
-      };
-      fetch('http://localhost:3000/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData),
-      })
-      .then(response => response.json())
-      .then(data => {
+    if (input.username !== "" && input.password !== "" && input.email !== "" && input.passwordCheck !== "" && passwordMatch()) {
+      try {
+        const formData = {
+          email: input.email,
+          username: input.username,
+          password: input.password,
+        };
+        const response = await fetch('http://localhost:3000/register', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        });
+  
+        if (!response.ok) {
+          throw new Error('Server responded with an error!');
+        }
+        const data = await response.json();
         alert(`What's good ${input.username}?!`);
-      })
-      .catch(() => {
+      } catch (error) {
         alert("There was an error processing your registration.");
-      });
+      }
     } else {
       alert("Lock in bro");
     }
   };
   
+  
   return (
-    <form onSubmit={handleSubmit} method="post">
-        <input type="email" name="email" placeholder="Email" onInput={handleInput}/>
-        <input type="name" name="username" placeholder="Username" onInput={handleInput}/>
-        <input type="password" name="password" placeholder="Password" onInput={handleInput}/>
-        <input type="password" name="passwordCheck" placeholder="Retype Password" onInput={handleInput}/>
+    <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" onChange={handleInput}/>
+        <input type="text" name="username" placeholder="Username" onChange={handleInput}/>
+        <input type="password" name="password" placeholder="Password" onChange={handleInput}/>
+        <input type="password" name="passwordCheck" placeholder="Retype Password" onChange={handleInput}/>
         <button type="submit">Register</button>
     </form>
   );
