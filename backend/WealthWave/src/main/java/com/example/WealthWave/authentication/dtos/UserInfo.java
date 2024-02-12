2c343914f18a627889997f5ee58b2af717e2e5ff
Nour Ajami <ajami.nour4@gmail.com>
2024-02-11 21:34:07 -0700

    Backend code for authentication, not 100% correct fow now.

--- backend/WealthWave/src/main/java/com/example/WealthWave/authentication/dtos/UserInfo.java
+++ backend/WealthWave/src/main/java/com/example/WealthWave/authentication/dtos/UserInfo.java
@@ -1,6 +1,7 @@
 package com.example.WealthWave.authentication.dtos;
 public record UserInfo(
-        String firstName,
+        String sub,
+        String name,
         String lastName,
         String email,
         boolean email_verified
