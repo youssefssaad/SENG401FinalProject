b5365314d67372dcf5ab4537987247e6f29c29e5
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2024-02-14 00:16:02 -0700

    expenses page initial layout- hardcoded

--- frontend/src/pages/expenses.js
+++ frontend/src/pages/expenses.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
 
 function Expenses() {
+  const [totalBudget, setTotalBudget] = useState(1000); // Set an initial total budget value
+  const [expenses, setExpenses] = useState({
+    rent: 500,
+    food: 200,
+    play: 100,
+    savings: 50,
+    investment: 150,
+  });
+
+  const handleBudgetChange = (amount) => {
+    setTotalBudget((prevBudget) => prevBudget + amount);
+  };
+
+  const handleExpenseChange = (category, amount) => {
+    setExpenses((prevExpenses) => ({
+      ...prevExpenses,
+      [category]: prevExpenses[category] + amount,
+    }));
+  };
+
   return (
-    <div>expenses</div>
+    <div>
+      <div>
+        <h1>Expenses</h1>
+      </div>
+
+    <div style={{alignContent: 'center'}}>
+    <div>Total Budget: ${totalBudget}</div>
+      <button onClick={() => handleBudgetChange(-50)}>Decrease Budget</button>
+      <button onClick={() => handleBudgetChange(50)}>Increase Budget</button>
+      <hr />
+      <div>
+        <ul>
+          {Object.entries(expenses).map(([category, amount]) => (
+            <li key={category}>
+              {category}:
+              <span style={{ color: amount > totalBudget ? "red" : "green" }}>
+                ${amount}
+              </span>
+              <button onClick={() => handleExpenseChange(category, -10)}>
+                Decrease
+              </button>
+              <button onClick={() => handleExpenseChange(category, 10)}>
+                Increase
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+      
+    </div>
   );
-};
+}
 
 export default Expenses;
