35a1353ee997c19bd7a1ecb767a8d83b3575e069
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2024-02-14 09:09:10 -0700

    styling to login, register, and expenses

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,17 +1,17 @@
-import React , {useState} from 'react';
+import React, { useState } from "react";
 // import GoogleSignIn from './GoogleSignIn';
 
 const Login = () => {
   const [input, setInput] = useState({
     username: "",
-    password: ""
-  })
+    password: "",
+  });
 
   // const handleSignInSuccess = (googleUser) => {
   //   const idToken = googleUser.getAuthResponse().id_token;
   //   console.log("User signed in successfully with token:", idToken);
   // };
-  
+
   // const handleSignInFailure = (error) => {
   //   console.error("Sign in failed", error);
   // };
@@ -26,42 +26,60 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (input.username !== "" && input.password !== "") {  
+    if (input.username !== "" && input.password !== "") {
       const formData = {
         username: input.username,
         password: input.password,
       };
-      fetch('http://localhost:3000/login', {
-        method: 'POST',
+      fetch("http://localhost:3000/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       })
-      .then(response => response.json())
-      .then(data => {
-        alert(`What's good ${input.username}?!`);
-      })
-      .catch(() => {
-        alert("There was an error processing your registration.");
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          alert(`What's good ${input.username}?!`);
+        })
+        .catch(() => {
+          alert("There was an error processing your registration.");
+        });
     } else {
       alert("Lock in bro");
     }
   };
 
   return (
-    <form onSubmit = {handleSubmit}>
-      <input type="text" name="username" placeholder="Username" onChange={handleInput}/>
-      <input type="password" name="password" placeholder="Password" onChange={handleInput}/>
-      <button type="submit">Login</button>
-      <p className='registeration'> Don't got an account?</p> 
-      <a href="/register" type='registration_link'>Sign up here!</a>
-      {/* <GoogleSignIn 
+    <>
+      <div classname="title">
+        <h1 classname="title">Login</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          onChange={handleInput}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleInput}
+        />
+        <button type="submit">Login</button>
+      
+        <p className="registeration"> Don't got an account?</p>
+        <a href="/register" type="registration_link">
+          Sign up here!
+        </a>
+        {/* <GoogleSignIn 
         onSignInSuccess={handleSignInSuccess} 
         onSignInFailure={handleSignInFailure} 
       /> */}
-    </form>
+      </form>
+    </>
   );
 };
 
