6ba79e9d1cd9c06bf8a4cc79c5791dbe1a0ffc3b
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2024-03-15 02:10:23 -0600

    fixing my merge mistake

--- frontend/src/pages/Expenses.js
+++ frontend/src/pages/Expenses.js
@@ -1,59 +1,130 @@
-import React, { useState } from "react";
-import Navbar from "../components/Navbar";
+import React, { useState, useEffect } from "react";
+import "../index.css";
+import Modal from "../components/Modal";
+import questionIcon from "../assets/question_icon.png";
+import Instruction from "../components/Instruction";
 
 function Expenses() {
-  const [totalBudget, setTotalBudget] = useState(1000); // Set an initial total budget value
-  const [expenses, setExpenses] = useState({
-    rent: 500,
-    food: 200,
-    play: 100,
-    savings: 50,
-    investment: 150,
-  });
+  const [showModal, setShowModal] = useState(false);
+  const [showInstruction, setShowInstruction] = useState(true);
+  const [goalFields, setGoalFields] = useState([]);
+  const [expenses, setExpenses] = useState({});
+  const [totalBudget, setTotalBudget] = useState(0);
+  const [tempExpenses, setTempExpenses] = useState({});
 
-  const handleBudgetChange = (amount) => {
-    setTotalBudget((prevBudget) => prevBudget + amount);
+  const openModal = () => {
+    setShowModal(true);
   };
 
-  const handleExpenseChange = (category, amount) => {
-    setExpenses((prevExpenses) => ({
-      ...prevExpenses,
-      [category]: prevExpenses[category] + amount,
-    }));
+  const closeModal = () => {
+    setShowModal(false);
   };
+
+  const openInstruction = () => {
+    setShowInstruction(true);
+  };
+
+  const closeInstruction = () => {
+    setShowInstruction(false);
+  };
+
+  const updateExpensesFromGoals = () => {
+    const newExpenses = {};
+    goalFields.forEach(({ goal, percentage }) => {
+      const amount = (totalBudget * parseFloat(percentage)) / 100;
+      newExpenses[goal] = amount;
+    });
+    setExpenses(newExpenses);
+    setTempExpenses(newExpenses); // Initialize tempExpenses with expenses
+  };
+
+  const getGoalsFromLocalStorage = () => {
+    const savedGoals = JSON.parse(localStorage.getItem("goalFields")) || [];
+    setGoalFields(savedGoals);
+  };
+
+  const updateGoalsInExpenses = (updatedGoals, updatedTotalBudget) => {
+    setGoalFields(updatedGoals);
+    setTotalBudget(updatedTotalBudget); 
+    updateExpensesFromGoals();
+  };
+
+  const handleIncrease = (category) => {
+    const newTempExpenses = { ...tempExpenses };
+    newTempExpenses[category] += 1;
+    setTempExpenses(newTempExpenses);
+
+    // Check if total temporary expenses exceed total budget, and if so, alert the user
+    const tempTotal = Object.values(newTempExpenses).reduce(
+      (acc, curr) => acc + curr,
+      0
+    );
+    if (tempTotal > totalBudget) {
+      alert("Your spending has surpassed the budgeted amount for this month.");
+    }
+  };
+
+  const handleDecrease = (category) => {
+    const newTempExpenses = { ...tempExpenses };
+    if (newTempExpenses[category] > 0) {
+      newTempExpenses[category] -= 1;
+      setTempExpenses(newTempExpenses);
+    }
+  };
+
+  useEffect(() => {
+    getGoalsFromLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    updateExpensesFromGoals();
+  }, [goalFields, totalBudget]);
 
   return (
     <div>
-      <Navbar />
       <div>
         <h1>Expenses</h1>
       </div>
 
-    <div style={{alignContent: 'center'}}>
-    <div>Total Budget: ${totalBudget}</div>
-      <button onClick={() => handleBudgetChange(-50)}>Decrease Budget</button>
-      <button onClick={() => handleBudgetChange(50)}>Increase Budget</button>
-      <hr />
-      <div>
-        <ul>
-          {Object.entries(expenses).map(([category, amount]) => (
-            <li key={category}>
-              {category}:
-              <span style={{ color: amount > totalBudget ? "red" : "green" }}>
-                ${amount}
-              </span>
-              <button onClick={() => handleExpenseChange(category, -10)}>
-                Decrease
-              </button>
-              <button onClick={() => handleExpenseChange(category, 10)}>
-                Increase
-              </button>
-            </li>
-          ))}
-        </ul>
+      <div className="container">
+        <div className="addButton addButton--active" onClick={openModal}></div>
+        <div className="instructions-modal" onClick={openInstruction}>
+          <img
+            src={questionIcon}
+            alt="Question Icon"
+            className="question-icon"
+          />
+        </div>{" "}
+        {showModal && (
+          <Modal
+            closeModal={closeModal}
+            updateGoalsInExpenses={updateGoalsInExpenses}
+            totalBudget={totalBudget}
+            goalFields={goalFields} 
+          />
+        )}
+        {showInstruction && <Instruction closeInstruction={closeInstruction} />}
+        <hr />
+        <div>
+          <ul>
+            {Object.entries(expenses).map(([category, amount]) => (
+              <li className="expenses-item-list" key={category}>
+                {category}
+                <button onClick={() => handleIncrease(category)}>+</button>
+                <span
+                  className="expenses-monetary-amount"
+                  style={{
+                    color: tempExpenses[category] > amount ? "red" : "green",
+                  }}
+                >
+                  ${tempExpenses[category].toFixed(2)}
+                </span>
+                <button onClick={() => handleDecrease(category)}>-</button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
-      
     </div>
   );
 }
