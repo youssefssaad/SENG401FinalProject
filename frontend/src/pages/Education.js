f9622f3046b33d107e973953c32583ca43aefd37
aryan312216 <aryan.sharma2@ucalgary.ca>
2024-03-21 20:48:27 -0600

    education page edited

--- frontend/src/pages/Education.js
+++ frontend/src/pages/Education.js
@@ -7,56 +7,41 @@ import insauranceImage from '../assets/InsauranceImage.jpeg';
 import investmentImage from '../assets/InvestmentImage.jpg';
 import retirementImage from '../assets/RetirementImage.jpg';
 import taxImage from '../assets/TaxImage.jpeg';
+import emergencyFundImage from '../assets/EmergencyFundsImage.jpg';
+
+// EducationItem component
+function EducationItem({ href, imgSrc, imgAlt, text }) {
+  return (
+    <div className="education-item">
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <img className="education-image" src={imgSrc} alt={imgAlt} />
+        <p>{text}</p>
+      </a>
+    </div>
+  );
+}
 
 function Education() {
+  const educationItems = [
+    { href: 'https://www.nerdwallet.com/ca/personal-finance/how-to-budget', imgSrc: budgetImage, imgAlt: 'Budgeting', text: 'Budgeting' },
+    { href: 'https://bettermoneyhabits.bankofamerica.com/en/saving-budgeting/ways-to-save-money', imgSrc: savingImage, imgAlt: 'Saving', text: 'Saving' },
+    { href: 'https://fortune.com/recommends/investing/how-to-start-investing/', imgSrc: investmentImage, imgAlt: 'Investing', text: 'Investing' },
+    { href: 'https://www.canada.ca/en/financial-consumer-agency/services/debt/plan-debt-free.html', imgSrc: debtManageImage, imgAlt: 'Debt Management', text: 'Debt Management' },
+    { href: 'https://www.investopedia.com/terms/r/retirement-planning.asp', imgSrc: retirementImage, imgAlt: 'Retirement Planning', text: 'Retirement Planning' },
+    { href: 'https://www.canada.ca/en/revenue-agency/programs/about-canada-revenue-agency-cra/understanding-taxes-benefits.html', imgSrc: taxImage, imgAlt: 'Taxes', text: 'Taxes' },
+    { href: 'https://www.ibc.ca/insurance-basics/how-insurance-works', imgSrc: insauranceImage, imgAlt: 'Insurance', text: 'Insurance' },
+    { href: 'https://www.wealthsimple.com/en-ca/learn/emergency-funds', imgSrc: emergencyFundImage, imgAlt: 'Emergency Fund', text: 'Emergency Fund' },
+  ];
+
   return (
     <div className="education">
       <Navbar />
       <h1>Education</h1>
       <p className="education-intro">Click on any of these resources to help you learn more about personal finance:</p>
       <div className="education-links">
-        <div className="education-item">
-          <a href="https://www.nerdwallet.com/ca/personal-finance/how-to-budget" target="_blank" rel="noopener noreferrer">
-            <img className="education-image" src={budgetImage} alt="Budgeting" />
-            <p>Budgeting</p>
-          </a>
-        </div>
-        <div className="education-item">
-          <a href="https://bettermoneyhabits.bankofamerica.com/en/saving-budgeting/ways-to-save-money" target="_blank" rel="noopener noreferrer">
-            <img className="education-image" src={savingImage} alt="Saving" />
-            <p>Saving</p>
-          </a>
-        </div>
-        <div className="education-item">
-          <a href="https://fortune.com/recommends/investing/how-to-start-investing/" target="_blank" rel="noopener noreferrer">
-            <img className="education-image" src={investmentImage} alt="Investing" />
-            <p>Investing</p>
-          </a>
-        </div>
-        <div className="education-item">
-          <a href="https://www.canada.ca/en/financial-consumer-agency/services/debt/plan-debt-free.html" target="_blank" rel="noopener noreferrer">
-            <img className="education-image" src={debtManageImage} alt="Debt Management" />
-            <p>Debt Management</p>
-          </a>
-        </div>
-        <div className="education-item">
-          <a href="https://www.investopedia.com/terms/r/retirement-planning.asp" target="_blank" rel="noopener noreferrer">
-            <img className="education-image" src={retirementImage} alt="Retirement Planning" />
-            <p>Retirement Planning</p>
-          </a>
-        </div>
-        <div className="education-item">
-          <a href="https://www.canada.ca/en/revenue-agency/programs/about-canada-revenue-agency-cra/understanding-taxes-benefits.html" target="_blank" rel="noopener noreferrer">
-            <img className="education-image" src={taxImage} alt="Taxes" />
-            <p>Taxes</p>
-          </a>
-        </div>
-        <div className="education-item">
-          <a href="https://www.ibc.ca/insurance-basics/how-insurance-works" target="_blank" rel="noopener noreferrer">
-            <img className="education-image" src={insauranceImage} alt="Insurance" />
-            <p>Insurance</p>
-          </a>
-        </div>
+        {educationItems.map((item, index) => (
+          <EducationItem key={index} {...item} />
+        ))}
       </div>
     </div>
   );
