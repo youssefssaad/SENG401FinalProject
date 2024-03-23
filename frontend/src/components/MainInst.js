34f038777336c2794d3b24ca4d17a6235b80161f
Youssef Saad <113144511+youssefssaad@users.noreply.github.com>
2024-03-23 04:31:51 -0600

    fontawesome

--- frontend/src/components/MainInst.js
+++ frontend/src/components/MainInst.js
@@ -8,27 +8,27 @@ const sections = [
   {
     icon: faGlobe,
     title: "Navigate to this website",
-    description: "Your money’s always making more with low-fee investing and high-interest savings."
+    description: "This is the easiest and most important step."
   },
   {
     icon: faSignInAlt,
     title: "Click the button above to sign up or log in",
-    description: "Get sophisticated investment opportunities traditionally reserved for industry insiders and the ultra-wealthy."
+    description: "Increase your chances of savings. We all know how hard it is to blindly manage money."
   },
   {
     icon: faTasks,
     title: "Set your goals by filling out the expenses page with all your percentages",
-    description: "In just a few taps, set your financial goals in motion, and let our easy-to-use products handle the rest."
+    description: "We know numbers get difficult past 74, so why not use a big pie chart. Who doesn't like pie? (me) :("
   },
   {
     icon: faEye,
     title: "Keep an eye on your budget summaries in the budget page",
-    description: "Maximize your earnings with our innovative financial tools and expert advice."
+    description: "Keep monitoring your progress!"
   },
   {
     icon: faGraduationCap,
-    title: "Since we know learning is always the key to success, checkout the education page for articles",
-    description: "Empower your financial knowledge with our comprehensive educational resources."
+    title: "As someone said, learning is great!!",
+    description: "Since we know learning is always the key to success, checkout the education page for articles"
   }
 ];
 
