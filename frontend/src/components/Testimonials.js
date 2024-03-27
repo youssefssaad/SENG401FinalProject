88683c069d08edf7e58dfac440d87439780fa9ee
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2024-03-27 11:05:02 -0600

    styling the expenses list

--- frontend/src/components/Testimonials.js
+++ frontend/src/components/Testimonials.js
@@ -1,26 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Testimonials = () => {
   return (
     <div className="main-page-test">
-    <h2>Testimonials</h2>
-    <p>Too lazy to sign up? Just read the testimonials below and you'll change your mind.</p>
-    <ul>
-      <li>"Since I started using this app, managing my finances has become not just easier, but actually enjoyable. 
-        I've always struggled to keep track of my spending and stick to a budget, but the app's intuitive design and insightful 
-        analytics have turned what was once a daunting task into a simple, rewarding routine. I'm saving more money than ever before, 
-        and I feel fully in control of my financial future. Truly a game-changer!" - Anna Amery</li>
-      <li>"As a busy professional with little time to spare, I needed a straightforward solution for managing my personal finances without 
-        the hassle of spreadsheets or manual tracking. This app has been a revelation, offering quick, efficient budget management and 
-        transaction tracking that fits seamlessly into my hectic schedule. The feature to export budget allocation as a CSV has been 
-        particularly useful for financial planning and discussions with my advisor." - Mohammed George</li>
-      <li>"Starting out on my financial independence journey was intimidating, to say the least. This app has been an invaluable mentor, 
-        guiding me through the basics of budgeting, saving, and investing. The clear, user-friendly interface and helpful tips have 
-        made what seemed like a complex process completely manageable. I'm not only learning how to manage my money better but also 
-        understanding financial principles that will benefit me for a lifetime. I couldn't have asked for a better tool to start with." - Gary Bob</li>
-    </ul>
-  </div>
-  )
-}
+      <h2>Testimonials</h2>
+      <p>
+        Too lazy to sign up? Just read the testimonials below and you'll change
+        your mind.
+      </p>
+      <ul>
+        <li>
+          "Since I started using this app, managing my finances has become not
+          just easier, but actually enjoyable. I've always struggled to keep
+          track of my spending and stick to a budget, but the app's intuitive
+          design and insightful analytics have turned what was once a daunting
+          task into a simple, rewarding routine. I'm saving more money than ever
+          before, and I feel fully in control of my financial future. Truly a
+          game-changer!" - Anna Amery
+        </li>
+        <li>
+          "As a busy professional with little time to spare, I needed a
+          straightforward solution for managing my personal finances without the
+          hassle of spreadsheets or manual tracking. This app has been a
+          revelation, offering quick, efficient budget management and
+          transaction tracking that fits seamlessly into my hectic schedule. The
+          feature to export budget allocation as a CSV has been particularly
+          useful for financial planning and discussions with my advisor." -
+          Mohammed George
+        </li>
+        <li>
+          "Starting out on my financial independence journey was intimidating,
+          to say the least. This app has been an invaluable mentor, guiding me
+          through the basics of budgeting, saving, and investing. The clear,
+          user-friendly interface and helpful tips have made what seemed like a
+          complex process completely manageable. I'm not only learning how to
+          manage my money better but also understanding financial principles
+          that will benefit me for a lifetime. I couldn't have asked for a
+          better tool to start with." - Gary Bob
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Testimonials;
